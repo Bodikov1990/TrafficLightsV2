03a10c33ff2e9884aa05aab73a8186427053c6df
Bodikov Kuat <kuat.bodikov@gmail.com>
2021-12-09 21:11:06 +0500

    Created views for red, orange, green lights

--- TrafficLightsV2/ViewController.swift
+++ TrafficLightsV2/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum TrafficLightColors {
+    case redLight
+    case orangeLight
+    case greenLight
 }
 
+class ViewController: UIViewController {
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var orangeLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+   
+    @IBOutlet var startButton: UIButton!
+    
+    private let lightOff = 0.3
+    private let lightOn = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        
+        orangeLightView.layer.cornerRadius = orangeLightView.frame.width / 2
+        
+        greenLightView.layer.cornerRadius = greenLightView.frame.width / 2
+    }
+
+    @IBAction func startPressed() {
+        
+        
+    }
+    
+}
